fd4591558c10005af95c14136cf1cd92ad219a50
ncgordeev <ncgordeev@gmail.com>
2024-01-14 20:21:03 +0300

    added new method for url formating

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 ROOT_PATH = os.path.dirname(__file__)
 DATA_PATH = os.path.join(ROOT_PATH, "src", "data.json")
-SHINSERVICE = "https://www.shinservice.ru/catalog/tyres/diameter-is-20/profile-is-45/width-is-245/"
+SHINSERVICE = "https://www.shinservice.ru/catalog/tyres"
--- src/main_class.py
+++ src/main_class.py
@@ -2,7 +2,7 @@ import json
 
 import requests
 from bs4 import BeautifulSoup
-from config import DATA_PATH
+from config import DATA_PATH, SHINSERVICE
 
 
 class ShinserviceScraper:
@@ -30,4 +30,21 @@ class ShinserviceScraper:
     def save_data(cls, data: dict, file_path: str) -> None:
         with open(file_path, "a", encoding="utf-8") as file:
             json.dump(data, file, indent=2, ensure_ascii=False)
+
+    @staticmethod
+    def choose_tire_size(page_url: str):
+        tire_with = int(input("Введите ширину: "))
+        tire_high = int(input("Введите высоту профиля: "))
+        tire_diameter = int(input("Введите диаметр: "))
+        tire_season = input("Введите сезон: зима/лето: ").strip().lower()
+
+        if tire_season == "зима":
+            return (f"{page_url}/diameter-is-{tire_diameter}/profile-is-{tire_high}"
+                    f"/season-is-winter/width-is-{tire_with}/")
+        elif tire_season == "лето":
+            return (f"{page_url}/diameter-is-{tire_diameter}/profile-is-{tire_high}"
+                    f"/season-is-summer/width-is-{tire_with}/")
+        else:
+            print("Вы не выбрали сезонность. Загружены данные по всем сезонам.")
+            return f"{page_url}/diameter-is-{tire_diameter}/profile-is-{tire_high}/width-is-{tire_with}/"
 
