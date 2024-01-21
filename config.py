0039621f19420112967da96e1d7b67d52e826058
ncgordeev <ncgordeev@gmail.com>
2024-01-21 16:30:51 +0300

    refactored methods

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 ROOT_PATH = os.path.dirname(__file__)
 DATA_PATH = os.path.join(ROOT_PATH, "src", "data.json")
-SHINSERVICE = "https://www.shinservice.ru/catalog/tyres"
+SHINSERVICE = "https://www.shinservice.ru/catalog/tyres/"
--- src/main_class.py
+++ src/main_class.py
@@ -3,32 +3,13 @@ from json import JSONDecodeError
 
 import requests
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 from config import DATA_PATH, SHINSERVICE
 
 
-class ShinserviceScraper:
+class TyreScraper:
 
-    def __init__(self, title: str) -> None:
-        self.title = title
-
-    def scrape_page(self, page_url: str) -> None:
-        url = page_url
-        page_response = requests.get(url)
-        tires_dict = {}
-        if page_response.status_code == 200:
-            soup = BeautifulSoup(page_response.text, "html.parser")
-            tires = soup.find_all(class_="stp-catalog-card-goods")
-            for tire in tires:
-                title = tire.find(class_="stp-catalog-card-title").text.strip()
-                price = tire.find(class_="stp-price_1").text.strip()
-                tires_dict[title] = price
-            self.save_data(tires_dict, DATA_PATH)
-        else:
-            print("Failed to retrieve the webpage. Status code:", page_response.status_code)
-            exit()
-
-    @classmethod
-    def save_data(cls, data: dict, file_path: str) -> None:
+    def save_data(self, data: dict, file_path: str) -> None:
         try:
             with open(file_path, "r", encoding="utf-8") as file:
                 read_data = json.load(file)
@@ -40,20 +21,65 @@ class ShinserviceScraper:
         with open(file_path, "w", encoding="utf-8") as file:
             json.dump(read_data, file, indent=2, ensure_ascii=False)
 
-    @staticmethod
-    def choose_tire_size(page_url: str):
-        tire_with = int(input("Введите ширину: "))
-        tire_high = int(input("Введите высоту профиля: "))
-        tire_diameter = int(input("Введите диаметр: "))
-        tire_season = input("Введите сезон: зима/лето: ").strip().lower()
+    def get_data(self, file_path: str):
+        try:
+            with open(file_path, "r", encoding="utf-8") as file:
+                raw_data = json.load(file)
+                print(len(raw_data))  # may delete
+                for title, price in raw_data.items():
+                    print(f"{title} - {price}")
+        except JSONDecodeError:
+            print("Данные отсутствуют!")
 
-        if tire_season == "зима":
-            return (f"{page_url}/diameter-is-{tire_diameter}/profile-is-{tire_high}"
-                    f"/season-is-winter/width-is-{tire_with}/")
-        elif tire_season == "лето":
-            return (f"{page_url}/diameter-is-{tire_diameter}/profile-is-{tire_high}"
-                    f"/season-is-summer/width-is-{tire_with}/")
+
+class ShinserviceScraper(TyreScraper):
+
+    def scrape_page(self, page_url: str) -> None:
+        number_of_page = "?page="
+        page_count = 0
+        url = f"{page_url}{number_of_page}{page_count}"
+        user_agent = UserAgent()
+        add_user_agent = user_agent.random
+        headers = {
+            "User-Agent": add_user_agent,
+        }
+        page_response = requests.get(url, headers=headers)
+        main_url = "https://www.shinservice.ru"
+        tyres_dict = {}
+        if page_response.status_code == 200:
+            soup = BeautifulSoup(page_response.text, "html.parser")
+            pagination = soup.find_all(class_="stp-pagination-item")
+            for index in range(len(pagination) + 1):
+                page_count += 1
+                url = f"{page_url}{number_of_page}{page_count}"
+                page_response = requests.get(url, headers=headers)
+                soup = BeautifulSoup(page_response.text, "html.parser")
+
+                tyres = soup.find_all(class_="stp-catalog-card-goods")
+                for tyre in tyres:
+                    title = tyre.find(class_="stp-catalog-card-title").text.strip()
+                    price = tyre.find(class_="stp-price_1").text.strip().replace(" ", "")
+                    raw_link = tyre.find(class_="title__Link-sc-12e08d15-1 cQLJhK")
+                    tyre_link = f"{main_url}{raw_link.get('href')}"
+                    tyres_dict[title] = price, tyre_link
+                self.save_data(tyres_dict, DATA_PATH)
+        else:
+            print("Failed to retrieve the webpage. Status code:", page_response.status_code)
+            exit()
+
+    @staticmethod
+    def choose_tyre_size(page_url: str):
+        tyre_with = input("Введите ширину: ")
+        tyre_high = input("Введите высоту профиля: ")
+        tyre_diameter = input("Введите диаметр: ")
+        tyre_season = input("Введите сезон: зима/лето: ").strip().lower()
+
+        if tyre_season == "зима":
+            return (f"{page_url}/diameter-is-{tyre_diameter}/profile-is-{tyre_high}"
+                    f"/season-is-winter/width-is-{tyre_with}/")
+        elif tyre_season == "лето":
+            return (f"{page_url}/diameter-is-{tyre_diameter}/profile-is-{tyre_high}"
+                    f"/season-is-summer/width-is-{tyre_with}/")
         else:
             print("Вы не выбрали сезонность. Загружены данные по всем сезонам.")
-            return f"{page_url}/diameter-is-{tire_diameter}/profile-is-{tire_high}/width-is-{tire_with}/"
-
+            return f"{page_url}/diameter-is-{tyre_diameter}/profile-is-{tyre_high}/width-is-{tyre_with}/"
